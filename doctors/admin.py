80e4de91e8b9cbe804b69bc4636d157edc88fd06
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-07-22 23:32:10 +0530

    Customized Doctor Admin Panel

--- doctors/admin.py
+++ doctors/admin.py
@@ -2,4 +2,22 @@ from django.contrib import admin
 from .models import Doctor     #from .models import Doctor
 
 # Register your models here.
-admin.site.register(Doctor)     #by registering the model your are telling django : Shoe the Doctor model in admin .
+@admin.register(Doctor)    #by registering the model your are telling django : Shoe the Doctor model in admin .
+class DoctorAdmin(admin.ModelAdmin):
+    list_display=(
+        "id",
+        "name",
+        "specialization",
+        "email",
+    )
+    search_fields=(
+        "name",
+        "specialization",
+        "email",
+    )
+    list_filter=(
+        "specialization",
+    )
+    ordering=(
+        "name",
+    )
