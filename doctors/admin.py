25a30bea990d84257306b72dc47f74a19c9ef322
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-07-22 21:26:43 +0530

    Added Doctor Model and Register in Django admin

--- doctors/admin.py
+++ doctors/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Doctor     #from .models import Doctor
 
 # Register your models here.
+admin.site.register(Doctor)     #by registering the model your are telling django : Shoe the Doctor model in admin .
--- doctors/models.py
+++ doctors/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models   
+# without importing models django wont able to create table.
+# after creating class run migration and migrate on cmd prompt 
 
 # Create your models here.
+class Doctor(models.Model):    # Django creates a database table Doctor name.
+    name=models.CharField(max_length=100)
+    specialization=models.CharField(max_length=100)
+    email=models.EmailField()
+    phone=models.CharField(max_length=12)
+
+    def __str__(self):     #yeh current object ko print krta h withot this you will see doctor object 1
+        return self.name
+    
+
