e34ffd624b95e849ca5ddcad36cfb1c996e91861
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-07-24 21:27:12 +0530

    Successfully completed Doctors Module and CRUD working correctly

--- doctors/urls.py
+++ doctors/urls.py
@@ -4,4 +4,6 @@ from . import views   # . means current app(doctors).  so we are importing views
 urlpatterns=[
     path("",views.doctor_list,name="doctor_list"),  #this means /doctors/    will call doctor_list()
     path("add/", views.doctor_add,name="doctor_add"),
+    path("edit/<int:pk>/",views.doctor_edit,name="doctor_edit"),
+     path("delete/<int:pk>/",views.doctor_delete,name="doctor_delete"),
 ]
--- doctors/views.py
+++ doctors/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Doctor  #we are importing Doctor model without this we cannot access databse table
 from .forms import DoctorForm
 # Create your views here.
@@ -13,11 +13,40 @@ def doctor_list(request):
                   )
 
 def doctor_add(request):
+    if request.method =="POST":
+        form=DoctorForm(request.POST)
 
-    form=DoctorForm()
+        if form.is_valid():
+            form.save()
+            return redirect("doctor_list")
+    else:
+     form=DoctorForm()
     return render(
         request,"doctors/doctor_form.html",{"form":form}
     )
+
+def doctor_edit(request,pk):
+   doctor=get_object_or_404(Doctor,pk=pk)
+   if request.method=="POST":
+      form=DoctorForm(request.POST,instance=doctor)
+
+      if form.is_valid():
+         form.save()
+         return redirect("doctor_list")
+   else:
+      form= DoctorForm(instance=doctor)
+   return render(request,"doctors/doctor_form.html",{"form":form})
+   
+
+
+
+def doctor_delete(request,pk):
+   doctor=get_object_or_404(Doctor,pk=pk)
+   if request.method=="POST":
+      doctor.delete()
+      return redirect("doctor_list")
+
+   return render(request,"doctors/doctor_confirm_delete.html",{"doctor":doctor})
 
 
     
