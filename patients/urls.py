9dd0de79aae0df7f8233bc3881c96257caa117e1
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-07-29 11:52:00 +0530

    Succesfully Added edit and delete fuction in patients Module

--- patients/urls.py
+++ patients/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path("",views.patient_list,name="patient_list"),
     path("add/",views.patient_add,name="patient_add"),
+    path("edit/<int:pk>/",views.patient_edit,name="patient_edit"),
+    path("delete/<int:pk>/",views.patient_delete,name="patient_delete"),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import Patient
 from .forms import PatientForm
 
@@ -22,3 +22,25 @@ def patient_add(request):
     else:        
       form=PatientForm()
     return render(request,"Patients/patient_form.html",{"form":form})
+
+
+
+def patient_edit(request,pk):
+    patient=get_object_or_404(Patient,pk=pk)
+    if request.method=="POST":
+      form=PatientForm(request.POST,instance=patient)
+
+      if form.is_valid():
+         form.save()
+         return redirect("patient_list")
+    else:
+       form=PatientForm(instance=patient)  
+    return render(request,"Patients/patient_form.html",{"form":form}) 
+
+
+def patient_delete(request,pk):
+   patient=get_object_or_404(Patient,pk=pk)  
+   if request.method=="POST":
+      patient.delete()
+      return redirect("patient_list")
+   return render(request,"patients/patient_confirm_delete.html",{"patient":patient})
