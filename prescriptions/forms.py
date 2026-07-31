dde0422487c2bcfe284ce35d8f01c1c44f868266
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-07-31 12:58:51 +0530

    successfully created Edit and Delete feature and completed Prescription module

---
+++ prescriptions/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Prescription
+
+class PrescriptionForm(forms.ModelForm):
+    class Meta:
+        model=Prescription
+
+        fields=[
+            "appointment",
+            "diagnosis",
+            "medicines",
+            "dosage",
+            "instructions",
+        ]
--- prescriptions/urls.py
+++ prescriptions/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns=[
     path("",views.prescription_list,name="prescription_list"),
     path("add/",views.prescription_add,name="prescription_add"),
+    path("edit/<int:pk>/",views.prescription_edit,name="prescription_edit"),
+    path("delete/<int:pk>/",views.prescription_delete,name="prescription_delete"),
 ]
