87739a80939299b3e3c5fcdceece213bd7025b65
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-08-11 12:33:42 +0530

    successfully completed doctor-api

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
-from .views import PatientListAPIView,PatientDetailAPIView,DoctorListAPIView,PatientViewSet
+from .views import PatientListAPIView,PatientDetailAPIView,DoctorListAPIView,PatientViewSet,DoctorViewSet
 from rest_framework.routers import DefaultRouter
 
 
 
 router=DefaultRouter()
 router.register("patients-v2",PatientViewSet)
+router.register("doctors-v2",DoctorViewSet)
 
 urlpatterns=[
     path("patients/",PatientListAPIView.as_view(),name="patient-list"),
