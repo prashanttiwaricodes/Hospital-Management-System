8f5c7c42bc8235ecfdedbe2848d7f4995b3174dc
Prashant Tiwari <piyushtiwari71197@gmail.com>
2026-09-22 23:48:24 +0530

    fix Cors middleware spelling

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.Corsmiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
